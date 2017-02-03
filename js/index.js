560516bdf4936d2b1d840b53bcad08166223a9a5
ZAYY <806096961@qq.com>
2017-02-03 13:05:21 +0100

    preload

--- js/index.js
+++ js/index.js
@@ -46,6 +46,7 @@ $(document).ready(function(){
     $('#fifth').parallax("100%", 0);
     $('#sixth').parallax("100%", 0);
     $('#seventh').parallax("100%", 0);
+
     // $('#second').parallax("100%", 0);
     // $('#third').parallax("100%", 0);
 
